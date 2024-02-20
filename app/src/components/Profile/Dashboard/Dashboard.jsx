4d00cc9bbb6e393a40457941ed7d6615373e5c0c
https://github.com/davidhakobyanf/auro.git <hakobyan.david2003@gmail.com>
2024-02-20 20:10:42 +0300

    newcommit

--- app/src/components/Profile/Dashboard/Dashboard.jsx
+++ app/src/components/Profile/Dashboard/Dashboard.jsx
@@ -2,28 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import css from './Dashboard.module.css';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
-import { Button } from 'antd';
-import Cards from 'react-credit-cards-2';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { useFetching } from "../../../hoc/fetchingHook";
 import clientAPI from "../../../api/api";
-import LoadingSpin from "../../../hoc/LoadingSpin";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import { Chart } from "react-google-charts";
 import { useProfileData } from "../../../context/ProfileDataContext";
-import Table from "antd/lib/table";
-import {CaretDownOutlined, CaretUpOutlined, DeleteOutlined, EditOutlined} from "@ant-design/icons";
-
 const Dashboard = () => {
     const { profileDataList, setProfileDataList } = useProfileData();
-
-
-
     const fetchProfile = async () => {
         try {
             const { data: res } = await clientAPI.getProfile();
@@ -34,19 +17,9 @@ const Dashboard = () => {
             console.error('Error fetching profile:', error);
         }
     };
-
     useEffect(() => {
         fetchProfile();
     }, []);
-
-
-
-
-
-
-
-
-
     return (
         <div className={css.dashboard}>
             <div className={css.header}>
